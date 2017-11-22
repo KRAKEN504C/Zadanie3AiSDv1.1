c98a090109bf627a83631c747a433eec46566ae2
KRAKEN504C <32463878+KRAKEN504C@users.noreply.github.com>
2017-11-22 10:03:39 +0100

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <Windows.h>
+#include <string>
+
+using namespace::std;
+
+int main()
+{
+	string tab[20000] = {};
+
+	for (int x = 0; x <= 20000; x++)
+	{
+		cout << tab[x];
+	}
+
+
+
+
+
+	return 0;
+}
